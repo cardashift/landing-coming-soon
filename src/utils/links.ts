fa0ee87db65202a88eb3463fb7219252196f6220
N3UTR1NOS <N3UTR1NOS@gmail.com>
2021-12-10 22:58:49 +0100

    Fix discord link

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,4 +1,4 @@
-export const DISCORD_LINK = 'https://discord.gg/MZTtk9tV'
+export const DISCORD_LINK = 'https://discord.gg/CjGCRTp6Y2'
 export const TWITTER_LINK = 'https://twitter.com/cardashift';
 export const MEDIUM_LINK = 'https://cardashift.medium.com/';
 export const LINKEDIN_LINK = 'https://www.linkedin.com/company/cardashift';
